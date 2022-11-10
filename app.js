2e08fd47ad7029373bd85333b139e9d0c90857d1
nmedic <nmedicm@gmail.com>
2022-11-09 21:26:52 -0400

    correccion 9nov 1026pm

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 
 const inputTextoCantidad = document.querySelector("#inputTextoCantidad");
-const inputTextoColor = document.querySelector("#inputTextoColor");
-const inputValor = document.querySelector("#inputValor");
+const inputValor = 400000;
 const btnCalcular = document.querySelector("#btnCalcular");
 const inputTotal = document.querySelector("#inputTotal");
+const circle = document.querySelector("#circle");
+const selector = document.querySelector("#selector");
 const inputCantidadTotal = document.querySelector("#inputCantidadTotal")
-const multiplicacion = inputTextoCantidad.value * inputValor
+
+
+selector.addEventListener("change", () => {
+    circle.style.backgroundColor = selector.value;
+})
 
 btnCalcular.addEventListener("click", () => {
-    inputTotal.textContent = multiplicacion
+    inputTotal.textContent = (inputTextoCantidad.value * inputValor);
     inputCantidadTotal.textContent = inputTextoCantidad.value;
+
 })
